0e2685ccdc11246fdb4559b47f355236ef15f708
priyansh18 <singhalpriyansh58@gmail.com>
2021-04-06 20:51:47 +0530

    Error in Contact Model Fixed

--- mycontacts/models.py
+++ mycontacts/models.py
@@ -7,7 +7,7 @@ class Contact(models.Model):
   manager    = models.ForeignKey(User,on_delete=models.CASCADE,default=None)
   name        = models.CharField(max_length=20)
   email        = models.EmailField(max_length=100)
-  phone        = models.IntegerField()
+  phone        = models.IntegerField(max_length=10)
   info        = models.CharField(max_length=30)
   gender        = models.CharField(max_length=50,choices=(('male','Male'),('female','Female')))
   image        = models.ImageField(upload_to='images/',blank=True)
